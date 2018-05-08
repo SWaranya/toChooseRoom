2e4fce6a3b804a25907dbc0ba1d5b588fdf93b18
DESKTOP-BBNUM9B\S.WARANYA <wrya.sm@kkumail.com>
2018-05-08 15:11:33 +0700

    Initial commit

---
+++ app/src/main/java/com/example/swaranya/tochooseroom/GetRoomActivity.java
@@ -0,0 +1,13 @@
+package com.example.swaranya.tochooseroom;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GetRoomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_room);
+    }
+}
